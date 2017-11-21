8eb1d4e037982b5e76784ff367048fde47576b48
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-21 09:21:38 -0500

    sdf

--- people/src/people/Person.java
+++ people/src/people/Person.java
@@ -2,5 +2,46 @@ package people;
 
 public abstract class Person 
 {
-	asdfasd
+	private String familyName;
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	private String firstName;
+	private int age;
+	private double weight;
+
+	
+	public Person() 
+	
+
+	
 }
